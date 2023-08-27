9386906e6d2a4ab7fe115f6c5c081c806e2cae28
MD. Tamjid Bond <89590805+tamjidbond@users.noreply.github.com>
2023-08-27 17:06:32 +0600

    first

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+                clifford: '#E76F51',
+                tamjid: '#E76F51'
+              }
+      },
+    },
+    plugins: [],
+  }
